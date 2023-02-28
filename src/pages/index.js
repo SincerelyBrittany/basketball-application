c6a83e2a2d790094f3dabcbca33136b4e38dc3c8
SincerelyBrittany <buildwithbrittt@gmail.com>
2023-02-28 09:02:17 -0500

    creates PlaerCard component and moves player logic from display data to playercard

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@
 
 import Head from 'next/head'
 import { useState, useEffect } from 'react'
+import PlayerCard from "../components/PlayerCard";
 import Pagination from "../components/Pagination";
 import { paginate } from "../helpers/paginate";
 
@@ -51,40 +52,43 @@ export default function Home(props) {
     let team = teams.find(team => player.ta === team.ta);
     let teamImage = team.logo
 
-    return (<div
-      key={player.pid}
-      className="card"
-    >
-      <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-        <div class="md:flex">
-          <div class="md:w-2/3">
-            <img class="h-48 w-full object-cover md:h-200 md:w-300" src={player.headshot} alt="placeholder image" />
-          </div>
-          <div class="p-6 md:w-2/3 text-center">
-            <img src={teamImage} alt="team-image" class="ml-20 mb-4 shadow-lg rounded-full max-w-full h-10 align-middle border-none" />
-            <p>#{player.num} | {player.pos} </p>
-            <p>{player.fn} {player.ln}</p>
-            <p class="mt-2 text-gray-600"></p>
-          </div>
-        </div>
-        <div class="border-t-4 border-indigo-500 grid grid-cols-3 gap-1 text-center ">
-          <div class="rounded-md p-2">
-            <p>PPG </p>
-            <p>
-              {player.pts}
-            </p>
-          </div>
-          <div class=" rounded-md p-2">
-            <p>RPG </p>
-            <p>{player.reb}</p></div>
-          <div class=" rounded-md p-2">
-            <p>APG </p>
-            <p> {player.ast}</p>
-          </div>
-        </div>
-      </div>
-    </div>
+    return (
+      <PlayerCard player={player} team={team}/>
     )
+    // return (<div
+    //   key={player.pid}
+    //   className="card"
+    // >
+    //   <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+    //     <div class="md:flex">
+    //       <div class="md:w-2/3">
+    //         <img class="h-48 w-full object-cover md:h-200 md:w-300" src={player.headshot} alt="placeholder image" />
+    //       </div>
+    //       <div class="p-6 md:w-2/3 text-center">
+    //         <img src={teamImage} alt="team-image" class="ml-20 mb-4 shadow-lg rounded-full max-w-full h-10 align-middle border-none" />
+    //         <p>#{player.num} | {player.pos} </p>
+    //         <p>{player.fn} {player.ln}</p>
+    //         <p class="mt-2 text-gray-600"></p>
+    //       </div>
+    //     </div>
+    //     <div class="border-t-4 border-indigo-500 grid grid-cols-3 gap-1 text-center ">
+    //       <div class="rounded-md p-2">
+    //         <p>PPG </p>
+    //         <p>
+    //           {player.pts}
+    //         </p>
+    //       </div>
+    //       <div class=" rounded-md p-2">
+    //         <p>RPG </p>
+    //         <p>{player.reb}</p></div>
+    //       <div class=" rounded-md p-2">
+    //         <p>APG </p>
+    //         <p> {player.ast}</p>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+    // )
   });
 
   // https://mono.software/2020/07/29/equal-height-cards-with-flexbox/
